aa7a2bb7ce12f7b974ef027dcd40d2b91680f592
silvamaciel <macieljunio.si@gmail.com>
2025-04-27 16:17:30 -0300

    refactor:  add company para logica multiempresa

--- backend/models/DiscardReason.js
+++ backend/models/DiscardReason.js
@@ -7,14 +7,32 @@ const discardReasonSchema = new Schema(
     nome: {
       type: String,
       required: [true, "O nome do motivo de descarte é obrigatório."],
-      unique: true, // Garante que os motivos sejam únicos
       trim: true,
     },
+
+    company: {
+      type: Schema.Types.ObjectId,
+      ref: 'Company',
+      required: [true, 'A empresa é obrigatória para o motivo de descarte.'],
+      index: true
+  }
   },
+  
   {
     timestamps: true,
   }
 );
+
+
+discardReasonSchema.index({ company: 1, nome: 1 }, { unique: true });
+
+discardReasonSchema.post('save', function(error, doc, next) {
+  if (error.name === 'MongoServerError' && error.code === 11000 && error.keyPattern?.company && error.keyPattern?.nome) {
+      next(new Error(`O motivo de descarte com nome '${doc.nome}' já existe nesta empresa.`));
+  } else {
+      next(error);
+  }
+});
 
 const DiscardReason = mongoose.model("DiscardReason", discardReasonSchema);
 
