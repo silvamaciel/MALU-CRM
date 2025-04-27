80792144ce7c5fb8ffc76ca85dc91bdfa7c43c2e
silvamaciel <macieljunio.si@gmail.com>
2025-04-27 17:12:35 -0300

    feat: adicionando default True

--- backend/models/DiscardReason.js
+++ backend/models/DiscardReason.js
@@ -12,25 +12,37 @@ const discardReasonSchema = new Schema(
 
     company: {
       type: Schema.Types.ObjectId,
-      ref: 'Company',
-      required: [true, 'A empresa é obrigatória para o motivo de descarte.'],
-      index: true
-  }
+      ref: "Company",
+      required: [true, "A empresa é obrigatória para o motivo de descarte."],
+      index: true,
+    },
+    ativo: {
+      type: Boolean,
+      default: true,
+    },
   },
-  
+
   {
     timestamps: true,
   }
 );
 
-
 discardReasonSchema.index({ company: 1, nome: 1 }, { unique: true });
 
-discardReasonSchema.post('save', function(error, doc, next) {
-  if (error.name === 'MongoServerError' && error.code === 11000 && error.keyPattern?.company && error.keyPattern?.nome) {
-      next(new Error(`O motivo de descarte com nome '${doc.nome}' já existe nesta empresa.`));
+discardReasonSchema.post("save", function (error, doc, next) {
+  if (
+    error.name === "MongoServerError" &&
+    error.code === 11000 &&
+    error.keyPattern?.company &&
+    error.keyPattern?.nome
+  ) {
+    next(
+      new Error(
+        `O motivo de descarte com nome '${doc.nome}' já existe nesta empresa.`
+      )
+    );
   } else {
-      next(error);
+    next(error);
   }
 });
 
