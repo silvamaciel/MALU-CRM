2f93274f7445da8b06731284703817fba6a85397
silvamaciel <macieljunio.si@gmail.com>
2025-05-03 11:16:44 -0300

    feat(login): nova rota pra login por email e senha criada

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -6,8 +6,7 @@ const authController = require('../controllers/authController');
 // Rota para receber o callback do Google Sign-In do frontend
 router.post('/google/callback', authController.googleCallback);
 
-// Adicione aqui rotas para /register e /login se implementar login com senha
 // router.post('/register', authController.registerUser);
-// router.post('/login', authController.loginUser);
+router.post('/login', authController.loginUser);
 
 module.exports = router;
