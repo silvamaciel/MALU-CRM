da584246cf6cf5101a90ccca6088685ebdd53a8b
silvamaciel <macieljunio.si@gmail.com>
2025-05-10 18:25:32 -0300

    fix: faltou importar useEffect

--- frontend/src/pages/Integrations/IntegrationsPage.js
+++ frontend/src/pages/Integrations/IntegrationsPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useEffect, useCallback } from 'react';
 import { useGoogleLogin } from "@react-oauth/google";
 import { sendGoogleAuthCode } from "../../api/auth";
 import { toast } from "react-toastify";
