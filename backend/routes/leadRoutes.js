c94994cf8530b788a68439069a4aa60533a36f8b
silvamaciel <macieljunio.si@gmail.com>
2025-06-17 11:49:50 -0300

    Refactor(LeadRoutes)
    Retirar o protect, desnecessárioa qui

--- backend/routes/leadRoutes.js
+++ backend/routes/leadRoutes.js
@@ -17,7 +17,7 @@ router.get('/:id', protect, LeadController.getLeadById);
 router.put('/descartar/:id', protect, LeadController.descartarLead); 
 router.get('/:id/history',  protect, LeadController.getLeadHistory);
 router.post('/importar-csv', protect, upload.single('csvfile'), LeadController.importLeadsFromCSVController);
-router.get('/csv-template', protect, LeadController.downloadCSVTemplateController);
+router.get('/csv-template', LeadController.downloadCSVTemplateController);
 
 
 
