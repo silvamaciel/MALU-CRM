83e55d3aeb129e4c05222d9e4a4c2967974c82f1
silvamaciel <macieljunio.si@gmail.com>
2025-06-21 19:15:38 -0300

    Feat(SibebarlOGO)

--- frontend/src/components/Sidebar/Sidebar.js
+++ frontend/src/components/Sidebar/Sidebar.js
@@ -22,7 +22,7 @@ function Sidebar({ userData, handleLogout, closeMobileSidebar }) {
     return (
         <aside className="sidebar">
             <div className="sidebar-header">
-                <img src={maluIcon} alt="Logo" style={{ height: "98px", margin: "0 auto" }} />
+                <img src={maluIcon} alt="Logo" style={{ height: "98px" }} />
             </div>
             <nav className="sidebar-nav">
                 <ul>
