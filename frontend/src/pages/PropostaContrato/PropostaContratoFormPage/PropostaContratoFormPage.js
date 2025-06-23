145f1bbb362ad96db03e5579f6f23ed178c0d2a3
silvamaciel <macieljunio.si@gmail.com>
2025-06-23 12:32:35 -0300

    Refactor(PropostaContratoModal)

--- frontend/src/pages/PropostaContrato/PropostaContratoFormPage/PropostaContratoFormPage.js
+++ frontend/src/pages/PropostaContrato/PropostaContratoFormPage/PropostaContratoFormPage.js
@@ -255,11 +255,20 @@ function PropostaContratoFormPage() {
   <div className="modal-overlay">
     <div className="modal-content">
       <h2>Confirmar envio</h2>
-      <p>O valor da proposta é <strong>R$ {parseFloat(formData.valorPropostaContrato).toLocaleString('pt-BR')}</strong>.<br />
-         O valor de tabela é <strong>R$ {parseFloat(reservaBase?.tipoImovel === 'Unidade'
-            ? reservaBase?.imovel?.precoTabela
-            : reservaBase?.imovel?.preco).toLocaleString('pt-BR')}</strong>.<br />
-         Deseja continuar com o envio?</p>
+      <p>
+            O valor total da proposta (entrada + parcelas) é <strong>
+                R$ {(
+                (parseFloat(formData.valorEntrada) || 0) +
+                (formData.planoDePagamento || []).reduce((acc, p) => acc + (parseFloat(p.valorUnitario) || 0), 0)
+                ).toLocaleString('pt-BR')}
+            </strong>.<br />
+            O valor de tabela é <strong>
+                R$ {parseFloat(reservaBase?.tipoImovel === 'Unidade'
+                ? reservaBase?.imovel?.precoTabela
+                : reservaBase?.imovel?.preco).toLocaleString('pt-BR')}
+            </strong>.<br />
+            Deseja continuar com o envio?
+            </p>
       <div className="modal-buttons">
         <button className="cancel-button" onClick={() => setShowConfirmModal(false)}>Cancelar</button>
         <button className="confirm-button" onClick={async () => {
