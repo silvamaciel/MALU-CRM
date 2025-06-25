2ba46171196c53d084c81f3911a7072d5774f91e
silvamaciel <macieljunio.si@gmail.com>
2025-06-25 01:21:02 -0300

    Feat(AgendaAp)

--- frontend/src/pages/AgendaPage/AgendaPage.js
+++ frontend/src/pages/AgendaPage/AgendaPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { getEventosAgenda, criarEventoAgenda, deletarEventoAgenda } from "../api/agendaApi";
+import { getEventosAgenda, criarEventoAgenda, deletarEventoAgenda } from "../../api/agendaApi";
 import './AgendaPage.css';
 
 const AgendaPage = () => {
