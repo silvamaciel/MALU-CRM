ab9678878c30fe7c9d14e8ab537be82d3d4e99fd
silvamaciel <macieljunio.si@gmail.com>
2025-07-21 23:13:19 -0300

    Fix( Bug authorize)

--- backend/routes/integrationRoutes.js
+++ backend/routes/integrationRoutes.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const integrationController = require('../controllers/integrationController');
-const { protect } = require('../middlewares/authMiddleware'); // Apenas protect é necessário aqui
+const { protect, authorize } = require('../middlewares/authMiddleware');
 
 // POST /api/integrations/facebook/connect-page
 // Recebe o ID da página selecionada e o User Access Token do frontend
