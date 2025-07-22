4db2cbd168ce26ef8e0c29de944e783749844c12
silvamaciel <macieljunio.si@gmail.com>
2025-07-22 13:02:50 -0300

    Ajuste QrCode

--- frontend/src/components/GenerateQRCodeModal/GenerateQRCodeModal.js
+++ frontend/src/components/GenerateQRCodeModal/GenerateQRCodeModal.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { getEvolutionInstanceStatusApi } from '../../api/integrations';
-import './GenerateQRCodeModal.css';
 
 function GenerateQRCodeModal({ isOpen, onClose, instance }) {
     const [qrCode, setQrCode] = useState(null);
