046a14cfdd44f2eece483ee5b7ca9eaedc72770f
silvamaciel <macieljunio.si@gmail.com>
2025-08-08 12:23:55 -0300

    Ajuste de import public controller

--- backend/routes/publicRoutes.js
+++ backend/routes/publicRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const PublicController = require('../controllers/PublicLeadController');
+const PublicController = require('../controllers/PublicController');
 
 
 // Rota para o parceiro se identificar
